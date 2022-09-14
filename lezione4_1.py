be792f62b30fddbb9cd72fe9fa979df532d42dc8
alessandra.vetri <vetri@olomedia.it>
2022-09-14 15:44:52 +0200

    lezione4_esercizio1

---
+++ lezione4_1.py
@@ -0,0 +1,8 @@
+name = input("Come ti chiami? ")
+eta = int(input("Quanti anni hai? "))
+
+if eta>=18:
+    print ("Benvenuto " + name + " sei maggiorenne")
+else:
+    print("Benvenuto " + name + " sei minorenne")
+
